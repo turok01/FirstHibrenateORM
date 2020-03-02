6f79f50e363402ce7b7cebbf825a9738df1f95bb
turok01 <toolbox@tut.by>
2020-03-02 23:48:22 +0300

    create my first DB with Hibernate. One user and two cars.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+import models.Auto;
+import models.User;
+import services.UserService;
+
 public class Main {
+    public static void main(String[] args){
+        UserService userService = new UserService();
+        User user = new User("Kristina",18);
+        userService.saveUser(user);
+        Auto ferrari = new Auto("Ferrari", "red");
+        ferrari.setUser(user);
+        user.addAuto(ferrari);
+        Auto ford = new Auto("Ford","black");
+        ford.setUser(user);
+        user.addAuto(ford);
+        userService.updateUser(user);
+    }
 }
+
